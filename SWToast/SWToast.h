47f9b740fe0b0aeaeb737a7e02b47be2a853d6a6
Seven Wang <seven.wang@supernovachina.com>
2019-06-19 14:03:19 +0800

    Initial Commit

---
+++ SWToast/SWToast.h
@@ -0,0 +1,19 @@
+//
+//  SWToast.h
+//  SWToast
+//
+//  Created by Supernova SanDick SSD on 2019/6/19.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWToast.
+FOUNDATION_EXPORT double SWToastVersionNumber;
+
+//! Project version string for SWToast.
+FOUNDATION_EXPORT const unsigned char SWToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWToast/PublicHeader.h>
+
+
